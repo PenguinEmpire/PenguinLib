c404c7e355e7620dc96a8e0d104ac2da990f31f3
amanomg8 <amanomg8@gmail.com>
2025-12-01 19:23:49 -0800

    Added joystick position class

---
+++ lib/src/main/java/org/example/models/JoystickPosition.java
@@ -0,0 +1,11 @@
+package org.example.models;
+
+public class JoystickPosition {
+    long x;
+    long y;
+
+    JoystickPosition(long xPos, long yPos) {
+        this.x = xPos;
+        this.y = yPos;
+    }
+}
